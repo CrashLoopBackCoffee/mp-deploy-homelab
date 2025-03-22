ce17dbeb84c0697b5fc26563c265565f4ce6daa1
Mike Pagel <mike@mpagel.de>
2025-03-22 13:16:02 +0100

    add paperless

---
+++ services/paperless/pulumi/__main__.py
@@ -0,0 +1,35 @@
+"""Paperless ng."""
+
+import pulumi as p
+import pulumi_kubernetes as k8s
+
+from paperless.app import create_paperless
+from paperless.model import ComponentConfig
+
+component_config = ComponentConfig.model_validate(p.Config().require_object('config'))
+
+k8s_stack = p.StackReference(f'{p.get_organization()}/kubernetes/{p.get_stack()}')
+kube_config = k8s_stack.get_output('kube-config')
+k8s_provider = k8s.Provider('k8s', kubeconfig=kube_config)
+
+fqdn = p.Output.concat(p.get_project(), '.', k8s_stack.get_output('app-sub-domain'))
+p.export('fqdn', fqdn)
+
+ns = k8s.core.v1.Namespace(
+    'paperless',
+    metadata={'name': 'paperless'},
+    opts=p.ResourceOptions(
+        provider=k8s_provider,
+        # protect namespace as the PVCs with the storage data are keeping track of the PVs,
+        # otherwise new PVs are created:
+        protect=p.get_stack() == 'prod',
+    ),
+)
+
+namespaced_provider = k8s.Provider(
+    'paperless-provider',
+    kubeconfig=kube_config,
+    namespace=ns.metadata.name,
+)
+
+create_paperless(component_config, fqdn, namespaced_provider)
---
+++ services/paperless/pulumi/paperless/model.py
@@ -0,0 +1,49 @@
+"""Configuration model."""
+
+import os
+
+import pulumi as p
+import pydantic
+
+
+class ConfigBaseModel(pydantic.BaseModel):
+    model_config = pydantic.ConfigDict(
+        alias_generator=lambda s: s.replace('_', '-'),
+        populate_by_name=True,
+        extra='forbid',
+    )
+
+
+class EnvVarRef(ConfigBaseModel):
+    envvar: str
+
+    @property
+    def value(self) -> p.Output[str]:
+        return p.Output.secret(os.environ[self.envvar])
+
+
+class PaperlessConfig(ConfigBaseModel):
+    version: str
+    port: pydantic.PositiveInt = 8000
+    data_size_gb: pydantic.PositiveInt
+    media_size_gb: pydantic.PositiveInt
+    media_smb_share: str = 'paperless-media'
+    consume_size_mb: pydantic.PositiveInt
+    consume_smb_share: str = 'paperless-consume'
+
+
+class RedisConfig(ConfigBaseModel):
+    version: str
+    port: pydantic.PositiveInt = 6379
+
+
+class EntraIdConfig(ConfigBaseModel):
+    tenant_id: str = '19d0fb13-2d87-4699-9ae2-6e431148a6ae'
+    client_id: str
+    client_secret: str
+
+
+class ComponentConfig(ConfigBaseModel):
+    paperless: PaperlessConfig
+    redis: RedisConfig
+    entraid: EntraIdConfig
