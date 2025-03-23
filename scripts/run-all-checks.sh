95cc4877aebecb0377e9b9f8f04f95e251388707
Mike Pagel <mike@mpagel.de>
2025-03-23 13:10:21 +0100

    driveby: split check scripts

--- scripts/run-all-checks.sh
+++ scripts/run-all-checks.sh
@@ -3,7 +3,5 @@
 set -x
 set -eu
 
-uv run ruff format
-uv run ruff check --fix
-uv run pyright --warnings
-uv run --directory services/proxmox/ansible ansible-lint -v inventory playbooks roles
+./scripts/run-ansible-checks.sh
+./scripts/run-python-checks.sh
