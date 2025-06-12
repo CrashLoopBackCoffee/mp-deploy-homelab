c5e3435bc9b204e9feca6391fa7fe7fd5b9e1840
Mike Pagel <mike@mpagel.de>
2025-06-12 07:27:31 +0200

    expose kube-state-metrics

--- services/kubernetes/pulumi/kubernetes/kube_state_metrics.py
+++ services/kubernetes/pulumi/kubernetes/kube_state_metrics.py
@@ -20,13 +20,51 @@ def ensure_kube_state_metrics(component_config: ComponentConfig, k8s_provider: k
     )
     k8s_opts = p.ResourceOptions(provider=namespaced_k8s_provider)
 
-    k8s.helm.v3.Release(
+    kube_state_metrics = k8s.helm.v3.Release(
         'kube-state-metrics',
         chart='kube-state-metrics',
         version=component_config.kube_state_metrics.version,
         repository_opts={'repo': 'https://prometheus-community.github.io/helm-charts'},
         values={
             'crds': {'enabled': True},
+        },
+        opts=k8s_opts,
+    )
+
+    service = kube_state_metrics.status.apply(
+        lambda s: k8s.core.v1.Service.get(
+            'kube-state-metrics',
+            f'{s.namespace}/{s.name}',
+            opts=k8s_opts,
+        )
+    )
+
+    fqdn = f'kube-state-metrics.{component_config.microk8s.sub_domain}'
+
+    k8s.apiextensions.CustomResource(
+        'ingress',
+        api_version='traefik.io/v1alpha1',
+        kind='IngressRoute',
+        metadata={
+            'name': 'ingress',
+        },
+        spec={
+            'entryPoints': ['websecure'],
+            'routes': [
+                {
+                    'kind': 'Rule',
+                    'match': p.Output.concat('Host(`', fqdn, '`)'),
+                    'services': [
+                        {
+                            'name': service.metadata.name,
+                            'namespace': service.metadata.namespace,
+                            'port': 'http',
+                        },
+                    ],
+                }
+            ],
+            # use default wildcard certificate:
+            'tls': {},
         },
         opts=k8s_opts,
     )
